0acd98fe161ad620271d1e02ed178cd810c6d683
Ryan D'Souza <rdsouza@kobo.com>
2019-03-25 21:00:23 -0400

    Commiting

---
+++ app/src/main/java/com/example/myfirstapp/DisplayMessageActivity.kt
@@ -0,0 +1,21 @@
+package com.example.myfirstapp
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.TextView
+
+class DisplayMessageActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_display_message)
+
+        val message = intent.getStringExtra(EXTRA_MESSAGE)
+        val textView = findViewById<TextView>(R.id.textView).apply{
+            text = message
+        }
+
+    }
+
+
+}
